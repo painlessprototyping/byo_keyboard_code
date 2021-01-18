634d3681936bc06a054821369f78747ee32602d4
Travis Bumgarner <travis.bumgarner@gmail.com>
2021-01-17 20:23:42 -0500

    Added spotify code

--- v25/circuitpython/byo25_keyboard_media_controls.py
+++ v25/circuitpython/byo25_keyboard_media_controls.py
@@ -13,8 +13,13 @@ import adafruit_matrixkeypad
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
+control_key = Keycode.SHIFT
+cc = ConsumerControl(usb_hid.devices)
 
 cols = [digitalio.DigitalInOut(x) for x in (board.D11, board.D12, board.D13)]
 rows = [digitalio.DigitalInOut(x) for x in (board.D9, board.D10)]
@@ -24,8 +29,18 @@ keys = (
 )
 keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
+KEYS = [
+    ConsumerControlCode.SCAN_NEXT_TRACK,
+    ConsumerControlCode.VOLUME_INCREMENT,
+    ConsumerControlCode.PLAY_PAUSE,
+    ConsumerControlCode.SCAN_PREVIOUS_TRACK,
+    ConsumerControlCode.VOLUME_DECREMENT,
+    ConsumerControlCode.MUTE,
+]
+
 while True:
     keys = keypad.pressed_keys
     if keys:
-        print(keys)
+        for key in keys:
+            cc.send(KEYS[key])
     time.sleep(0.1)
